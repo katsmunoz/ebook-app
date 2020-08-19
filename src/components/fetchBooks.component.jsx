962fe2f262732d7131264fa15dc98a07e22d8f6b
katsmunoz <katherine.smm@gmail.com>
2020-08-19 08:04:47 -0400

    adding responsive

--- src/components/fetchBooks.component.jsx
+++ src/components/fetchBooks.component.jsx
@@ -1,8 +1,18 @@
 import React, { Fragment } from 'react';
-import { Grid, Box } from '@material-ui/core';
+import { Grid, Box, Tooltip } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 import './fetchBooks.style.css';
 
 const FetchBooks = ({ result }) => {
+
+    const LightTooltip = withStyles((theme) => ({
+        tooltip: {
+          backgroundColor: theme.palette.common.white,
+          color: 'rgba(0, 0, 0, 0.87)',
+          boxShadow: theme.shadows[5],
+          fontSize: 15,
+        },
+      }))(Tooltip);
 
     return (
         <Fragment>
@@ -10,10 +20,12 @@ const FetchBooks = ({ result }) => {
             {
                 result.map((book, index) => (
                     <Grid sm={3} md={2} item key={index}>
+                        <LightTooltip title={book.volumeInfo.title} arrow placement="top">
                         <a href={book.volumeInfo.previewLink} target="_blank" rel="noopener noreferrer">
-                            <Box className="ebook-img" style={{ textAlign: "center" }} style={{ backgroundImage: `url(${book.volumeInfo.imageLinks.thumbnail})` }} >
+                            <Box className="ebook-img" style={{ textAlign: "center", marginBottom: 20, backgroundImage: `url(${book.volumeInfo.imageLinks.thumbnail})` }} >
                             </Box>
-                        </a>
+                        </a>          
+                        </LightTooltip>
                     </Grid>
                 ))
             }
